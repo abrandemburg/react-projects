be4435531c94711edd1e31591863537b4ebe65b2
abrandemburg <aanjos.brandemburg@gmail.com>
2019-07-11 23:38:21 -0300

    React boilerplate

---
+++ react-ninja/src/index.js
@@ -0,0 +1,24 @@
+'user strict'
+
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './app'
+
+const renderApp = (AnyApp) => {
+  render(
+    <AppContainer>
+      <AnyApp />
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  )
+}
+
+renderApp(App)
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
+    renderApp(NextApp)
+  })
+}
